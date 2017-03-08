3f239c8f8783aa7d281330c5d645d2d70de6e35d
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-03-08 17:34:58 +0000

    CoG with varying microturbulence

---
+++ figures/cog_vt.py
@@ -0,0 +1,23 @@
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+'''Curve of growth as function of vt'''
+
+
+if __name__ == '__main__':
+
+    vts = [0.5, 2.5, 5.0]
+    for i in range(1, 4):
+        gf, rw = np.loadtxt('cog_vt%s.dat' % i, unpack=True)
+        idx = np.argsort(gf)
+        gf, rw = gf[idx], rw[idx]
+        plt.plot(gf, rw, label=r'$\xi=%.2f$km/s' % vts[i-1])
+
+    plt.legend(loc='best', frameon=False)
+    plt.xlabel(r'log $gf$')
+    plt.ylabel(r'$\log(EW/\lambda)$')
+
+    # plt.savefig('cog_vt.pdf')
+    plt.show()
