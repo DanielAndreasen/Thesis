9950143c7c5631da076cc14b61942e3be46a7330
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-04-13 12:02:19 +0100

    Finish description on IRFM with a figure and some simple calc.

---
+++ figures/IRFM/IRFM.py
@@ -0,0 +1,42 @@
+from __future__ import division
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from astropy import constants as c
+
+
+def solar_radius(flux, fluxE):
+    R = c.R_sun.value
+    d = c.au.value
+    return 2*np.sqrt(fluxE/flux) * d / (2*R)
+
+
+def solar_teff(flux, fluxE):
+    def angD(flux, fluxE):
+        return 2*np.sqrt(fluxE/flux)
+    theta = angD(flux, fluxE)
+    sigma = c.k_B.value
+    Teff = ( 4*fluxE/(sigma*theta**2) )**(1/4)
+    return Teff.values
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('IRFM_Sun.csv')
+
+    rsun = solar_radius(df.Flux, df.FluxE)
+    Teff = solar_teff(df.Flux, df.FluxE)
+    print 'Solar radius: %.3f(%.3f)' % (np.mean(rsun), np.std(rsun))
+    print 'Solar Teff: %d(%d)' % (np.mean(Teff), np.std(Teff))
+
+
+    # Plot
+    plt.semilogy(df.w, df.Flux, 'o-', label='Calculated flux')
+    plt.semilogy(df.w, df.FluxE, 'o-', label='Measured flux at Earth')
+
+    plt.xlabel(r'Wavelength [$\mu$m]')
+    plt.ylabel(r'Flux [W m$^{-2}$ Hz$^{-1}$]')
+    plt.legend(loc='best', frameon=False)
+    plt.tight_layout()
+
+    # plt.savefig('../IRFM.pdf')
+    plt.show()
