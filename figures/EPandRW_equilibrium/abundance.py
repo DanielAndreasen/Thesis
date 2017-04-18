1cd3a6907b329ac9aa36380a8d789349c4cf8772
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-04-18 14:35:21 +0100

    Remove colours, larger y limit, and slopes

--- figures/EPandRW_equilibrium/abundance.py
+++ figures/EPandRW_equilibrium/abundance.py
@@ -2,6 +2,7 @@ from __future__ import division
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 from utils import Readmoog
 plt.rcParams['xtick.direction'] = 'in'
 plt.rcParams['ytick.direction'] = 'in'
@@ -37,42 +38,60 @@ def plot_data(fname, row, fig=None):
 
     # Left side (EP)
     ax1 = fig.add_subplot(321)
-    ax2 = fig.add_subplot(323, sharex=ax1)
-    ax3 = fig.add_subplot(325, sharex=ax1)
+    ax2 = fig.add_subplot(323, sharey=ax1)
+    ax3 = fig.add_subplot(325, sharey=ax1)
     # Right side (RW)
     ax4 = fig.add_subplot(322, sharey=ax1)
-    ax5 = fig.add_subplot(324, sharex=ax4, sharey=ax2)
-    ax6 = fig.add_subplot(326, sharex=ax4, sharey=ax3)
+    ax5 = fig.add_subplot(324, sharey=ax1)
+    ax6 = fig.add_subplot(326, sharey=ax1)
 
     # Use the right axes depending on the row
     if row == 1:
         ax1 = ax1
         ax2 = ax4
+        ax1.tick_params('x', labelcolor='w')
+        ax2.tick_params('both', labelcolor='w')
+        ax1.spines['top'].set_color('none')
+        ax1.spines['right'].set_color('none')
+        ax2.spines['top'].set_color('none')
+        ax2.spines['right'].set_color('none')
     elif row == 2:
         ax1 = ax2
         ax2 = ax5
+        ax1.tick_params('x', labelcolor='w')
+        ax2.tick_params('both', labelcolor='w')
+        ax1.spines['top'].set_color('none')
+        ax1.spines['right'].set_color('none')
+        ax2.spines['top'].set_color('none')
+        ax2.spines['right'].set_color('none')
     elif row == 3:
         ax1 = ax3
         ax2 = ax6
-        ax.set_ylabel('Abundance')
+        ax2.tick_params('y', labelcolor='w')
+        ax1.spines['top'].set_color('none')
+        ax1.spines['right'].set_color('none')
+        ax2.spines['top'].set_color('none')
+        ax2.spines['right'].set_color('none')
+        ax.set_ylabel('Abundance\n ')
         ax1.set_xlabel(r'EP [eV]')
         ax2.set_xlabel(r'$\log(EW/\lambda)$')
 
+    ax1.set_ylim(6.5, 9.0)
+    ax2.set_ylim(6.5, 9.0)
     ax2.yaxis.set_label_position('right')
     ax2.set_ylabel(d[fname], rotation=270, va='bottom')
+    ax2.set_title(d[fname])
 
-    im = ax1.scatter(x1, y, c=np.abs(df.delavg))
+    ax1.plot(x1, y ,'o', alpha=0.6, color='C0')
     ax1.plot(x1, y1, '--', color='C3', alpha=0.5)
+    ax1.text(0.5, 8.25, r'$a_\mathrm{EP}$=%.3f' % a1)
 
-    ax2.scatter(x2, y, c=np.abs(df.delavg))
+    ax2.plot(x2, y ,'o', alpha=0.6, color='C2')
     ax2.plot(x2, y2, '--', color='C3', alpha=0.5)
+    ax2.text(-6.2, 8.25, r'$a_\mathrm{RW}$=%.3f' % a2)
 
-    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-    fig.subplots_adjust(right=0.8)
-    cbar = fig.colorbar(im, cax=cbar_ax)
-    cbar.set_label('Abundance deviation', rotation=270, va='bottom')
-    fig.tight_layout(rect=[0, 0, 0.85, 1])
-
+    if row == 3:
+        fig.tight_layout(rect=[0, 0, 0.85, 1])
     return fig
 
 
