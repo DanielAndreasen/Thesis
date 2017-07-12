c3f4f5f1738323531b014bc25f5379a7e33b91fc
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-07-12 10:36:15 +0100

    Include a RV time series and phase curve as an example

---
+++ figures/RVmethod/rv.py
@@ -0,0 +1,33 @@
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams['axes.spines.right'] = False
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['axes.linewidth'] = 2
+plt.rcParams['xtick.major.width'] = 2
+plt.rcParams['ytick.major.width'] = 2
+
+
+if __name__ == '__main__':
+    fname = 'EPIC-9792_SOPHIE.rdb'
+    t, rv, drv = np.loadtxt(fname, skiprows=2, unpack=True)
+    p = 3.26083740
+    phase = (t%p)/p
+    idx = np.argsort(phase)
+
+    plt.subplot(211)
+    plt.errorbar(t, rv, yerr=drv, fmt='o')
+    plt.xlabel('Time')
+    plt.ylabel('RV [ km/s]')
+
+    plt.subplot(212)
+    plt.errorbar(phase[idx], rv[idx], yerr=drv[idx], fmt='o')
+    plt.xlabel('Phase, P=3.261 days')
+    plt.ylabel('RV [ km/s]')
+
+    plt.tight_layout()
+    # plt.savefig('../RVmethod.pdf')
+    plt.show()
