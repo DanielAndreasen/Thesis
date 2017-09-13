f28e334b6e349937678854fc47145b64fab87848
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-09-13 15:56:02 +0100

    Use abundance instead of loggf

--- figures/cog/cog.py
+++ figures/cog/cog.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
+from loggf2abundance import get_abundance
 plt.rcParams['xtick.direction'] = 'in'
 plt.rcParams['ytick.direction'] = 'in'
 
@@ -9,27 +10,26 @@ plt.rcParams['ytick.direction'] = 'in'
 
 if __name__ == '__main__':
 
-    d = np.loadtxt('cog.dat')
-    idx = np.argsort(d[:, 0])
-    d = d[idx]
+    df = get_abundance()
 
-    loggf_points = [-4, -2, 0, 2.036]
-    ds = np.zeros((len(loggf_points), 2))
-    for i, loggf_point in enumerate(loggf_points):
-        idx = d[:, 0]-loggf_point == min(abs(d[:, 0]-loggf_point))
-        ds[i, 0] = d[idx, 0]
-        ds[i, 1] = d[idx, 1]
+    abundances = [6.2, 7.2, 8.2, 9.2]
+    ds = np.zeros((len(abundances), 2))
+    for i, abundance in enumerate(abundances):
+        abundance_diff = abs(df['abund']-abundance)
+        idx = abundance_diff == min(abundance_diff)
+        ds[i, 0] = df.loc[idx, 'abund']
+        ds[i, 1] = df.loc[idx, 'logRWin']
 
     plt.subplot(211)
-    plt.plot(d[:, 0], d[:, 1], '-', alpha=0.7)
+    plt.plot(df['abund'], df['logRWin'], '-', alpha=0.7)
     for i, di in enumerate(ds):
         plt.plot(di[0], di[1], 'o', c='C%s' % i)
-    plt.text(-4.6, -5.8, 'Weak line', rotation=30)
-    plt.text(-1.6, -4.3, 'Saturation', rotation=11)
-    plt.text(1.0, -3.0, 'Strong line', rotation=15)
-    plt.xlim(-5, 3)
-    plt.ylim(-7.2, -2.6)
-    plt.xlabel(r'log $gf$')
+    plt.text(6.0, -5.6, 'Weak line', rotation=30)
+    plt.text(7.5, -4.75, 'Saturation', rotation=11)
+    plt.text(9.0, -4.2, 'Strong line', rotation=15)
+    plt.xlim(5.9, 9.7)
+    plt.ylim(-6.1, -4.2)
+    plt.xlabel('Abundance')
     plt.ylabel(r'$\log(EW/\lambda)$')
     ax = plt.gca()
     ax.spines['top'].set_color('none')
